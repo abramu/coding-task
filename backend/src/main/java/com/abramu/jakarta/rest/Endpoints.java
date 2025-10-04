f91eb942f84f75489a2b23ccfe259cb6ec9dc1a7
abramu <141283215+abramu@users.noreply.github.com>
2025-10-04 11:24:30 +0200

    feat(backend): endpoint for splitting

--- backend/src/main/java/com/abramu/jakarta/rest/Endpoints.java
+++ backend/src/main/java/com/abramu/jakarta/rest/Endpoints.java
@@ -2,13 +2,16 @@ package com.abramu.jakarta.rest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
+import com.abramu.jakarta.services.CurrencySplitter;
 import com.abramu.jakarta.services.CurrencyValues;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("")
@@ -17,10 +20,20 @@ public class Endpoints {
     @Inject
     private CurrencyValues currencyValues;
 
+    @Inject
+    private CurrencySplitter currencySplitter;
+
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
     @Path("values")
     public List<BigDecimal> getCurrencyValues() {
         return currencyValues.getValues();
+    }
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Path("split")
+    public Map<String, Integer> splitTotal(@QueryParam("total") BigDecimal total) {
+        return currencySplitter.split(total, getCurrencyValues());
     }
 }
