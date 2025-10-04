486578242670bd7e5dca80c9eb40960793e6bfed
abramu <141283215+abramu@users.noreply.github.com>
2025-10-04 23:14:09 +0200

    feat(frontend): layout and components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, MatTableModule],
   templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('coding-task-frontend');
+  protected currentResult = [{ currencyValue: 20, amount: 1 }];
+  protected readonly columnsToDisplay = ['currencyValue', 'amount'];
 }
